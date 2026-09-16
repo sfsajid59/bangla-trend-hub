d7561268ef6259f35400790986ad3a22049a5454
banglatrendhub <142731344+sfsajid59@users.noreply.github.com>
2026-09-16 13:46:16 +0600

    Create supabase.js

---
+++ assets/supabase.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "cyhzgnrtdpxhpzcyqcdj";
+const SUPABASE_ANON_KEY = "sb_publishable_vNfvxmtXFzV9M4w049G9aA_i8I-o43d";
+
+const { createClient } = supabase;
+
+const supabaseClient = createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
